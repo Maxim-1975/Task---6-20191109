9a749b3e7b76abb215d173e1b7a4e5230d16957e
Maxim-1975 <mkolpakov@gmail.com>
2019-11-09 18:19:55 +0300

    Task 6

---
+++ Second-Solution-Attempt-2/Second-Solution-Attempt-2/tests/ContactCreationTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactCreationTests:TestBase
+    {
+        [Test]
+        public void ContactCreationTest()
+        {
+            app.navigator.OpenHomePage();
+            app.auth.Login(new AccountData("admin", "secret"));
+            app.navigator.GoToAccountPage();
+            InitContactData icd = new InitContactData();
+            icd.Firstname = "Maxim";
+            icd.Middlename = "Vladimirovitch";
+            icd.Lastename = "Kolpakov";
+            app.contact.InitContactCreation(icd);
+            app.contact.SubmitContactCreation();
+            app.navigator.ReturnHomePage();
+            app.auth.Logout();
+        }
+    }
+}
